c689512262369bda97755aeb8402f3051b8d9afd
lvaudor <lise.vaudor@ens-lyon.fr>
2020-10-07 13:23:51 +0200

    data is first argument

--- R/predict_rf.R
+++ R/predict_rf.R
@@ -1,11 +1,12 @@
 #'Runs model random forest object on new data
-#' @param obj_rf random forest object
+#
 #' @param data data with variables Q, rT_Q, S
+#' @param obj_rf random forest object
 #' @export
 #' @return Wdata completed with column W (waiting time between two wood occurrences)
 #' @examples
 
-predict_rf=function(obj_rf, newdata){
+predict_rf=function(newdata,obj_rf){
   newdata=newdata %>%
     dplyr::mutate(Ypred=predict(obj_rf,newdata))
   return(newdata)
