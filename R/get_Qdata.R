3f7d2014b81db9fed6f7aa26dc08108c43e0b819
lvaudor <lise.vaudor@ens-lyon.fr>
2020-10-15 16:12:38 +0200

    format time

--- R/get_Qdata.R
+++ R/get_Qdata.R
@@ -10,7 +10,7 @@ get_Qdata=function(Wdata, station, extratime=1200){
   t1=min(Wdata$Time,na.rm=TRUE)-lubridate::seconds(extratime)
   t2=max(Wdata$Time,na.rm=TRUE)+lubridate::seconds(extratime)
   Qdata=banqueHydro::bh_get_qtvar(station=station,
-                                  t1 %>% format_time_for_qtvar(),
-                                  t2 %>% format_time_for_qtvar())
+                                  t1 %>% banqueHydro::format_time_for_qtvar(),
+                                  t2 %>% banqueHydro::format_time_for_qtvar())
   return(Qdata)
 }
