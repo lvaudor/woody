e52dd46fadc0ece64f54cbadafa6514b878ff9dc
lvaudor <lise.vaudor@ens-lyon.fr>
2020-10-15 16:11:58 +0200

    vignette

--- vignettes/woody.R
+++ vignettes/woody.R
@@ -15,3 +15,8 @@ library(woody)
 ## ----import_Wdata, message=FALSE, warning=FALSE-------------------------------
 Wdat=import_Wdata(mypath) 
 
+## ----bh_get_qtvar-------------------------------------------------------------
+Qdat=get_Qdata(Wdata=Wdat,
+               station=mystation,
+               extratime=1200)
+
