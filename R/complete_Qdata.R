8ce0cab9c2a72209e51653d00810090ff426cb2e
lvaudor <lise.vaudor@ens-lyon.fr>
2021-01-26 16:56:28 +0100

    add qnorm and site to complete_Qdata

--- R/complete_Qdata.R
+++ R/complete_Qdata.R
@@ -3,14 +3,17 @@
 #' @param qtvar
 #' @return Qdata completed with variables T_Q, rT_Q and  S
 #' @examples
-complete_Qdata=function(qtvar){
+complete_Qdata=function(qtvar,qnorm,site){
   Qdata=qtvar %>%
+    dplyr::mutate(Q=Q/qnorm,
+                  site=rep(NA,n())) %>%
     dplyr::mutate(T_Q=purrr::map_dbl(Time,
                                      .f=calc_T_Q,
                                      Qdata=qtvar)) %>%
     dplyr::mutate(S=Q-approx(qtvar$Time,
                              qtvar$Q,
                              xout=qtvar$Time-60*5)$y) %>%
-    dplyr::mutate(rT_Q=sqrt(T_Q))
+    dplyr::mutate(rT_Q=sqrt(T_Q)) %>%
+    dplyr::select(site,Q,everything())
   return(Qdata)
 }
