03eadb22d4619a8e2e769ff2d67dccedfc8ea404
lvaudor <lise.vaudor@ens-lyon.fr>
2021-01-26 16:26:28 +0100

    complete Qdata no lookback

--- R/complete_Qdata.R
+++ R/complete_Qdata.R
@@ -1,21 +1,16 @@
-#' Completes Qdata with variables T_Q and S.
+#' Completes qtvar with variables T_Q and S. => Qdata
 #' @export
-#' @param Qdata
-#' @param Qdata_back
+#' @param qtvar
 #' @return Qdata completed with variables T_Q, rT_Q and  S
 #' @examples
-complete_Qdata=function(Qdata){
-  Qdata_back=get_Qdata_back(Qdata)
-  print("Calculating T_Q.")
-  Qdata=Qdata %>%
+complete_Qdata=function(qtvar){
+  Qdata=qtvar %>%
     dplyr::mutate(T_Q=purrr::map_dbl(Time,
                                      .f=calc_T_Q,
-                                     Qdata=Qdata,
-                                     Qdata_back=Qdata_back))
-  Qdata=Qdata %>%
-    dplyr::mutate(S=Q-approx(Qdata$Time,
-                             Qdata$Q,
-                             xout=Qdata$Time-60*5)$y) %>%
+                                     Qdata=qtvar)) %>%
+    dplyr::mutate(S=Q-approx(qtvar$Time,
+                             qtvar$Q,
+                             xout=qtvar$Time-60*5)$y) %>%
     dplyr::mutate(rT_Q=sqrt(T_Q))
   return(Qdata)
 }
