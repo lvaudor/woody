7efe63947914737d78e432efd32a7367d1d77dd2
lvaudor <lise.vaudor@ens-lyon.fr>
2021-01-27 14:08:03 +0100

    calc wind data

--- R/complete_Qdata_with_Vdata.R
+++ R/complete_Qdata_with_Vdata.R
@@ -1,22 +1,37 @@
-complete_Qdata_with_Vdata_onesite=function(Qdata,Vdata){
+#' Completes Qdata with variables Vmax,Vsum. => QVdata
+#' @export
+#' @param Qdata
+#' @param Vdata
+#' @return QVdata completed with variables Vmax,Vsum
+#' @examples
+complete_Qdata_with_Vdata=function(Qdata,Vdata){
   result=Qdata %>%
-    group_by(Date,T_Q) %>%
-    nest(Qdata=c(-Date,-T_Q)) %>%
-    mutate(Vdata=purrr::map2(.x=Date,.y=T_Q,.f=calc_V,Vdata)) %>%
-    unnest(cols=c(Date,T_Q,Qdata,Vdata)) %>%
-    ungroup()
+    dplyr::mutate(Date=lubridate::date(Time)) %>%
+    dplyr::mutate(Date_bef=Date-lubridate::days(round(T_Q))) %>%
+    dplyr::group_by(Date) %>%
+    tidyr::nest(data=c(Date,Date_bef,T_Q))
+  f_period=function(data){
+    if(is.na(data$Date_bef)){
+      return(tibble::tibble(Vmax=NA,Vsum=NA))
+    }
+    if(data$T_Q==0){
+      return(tibble::tibble(Vmax=Vdata %>%
+                              dplyr::filter(Date==data$Date) %>%
+                              dplyr::pull(FFM),
+                            Vsum=0))
+    }
+    res=Vdata %>%
+      dplyr::filter(Date>=data$Date_bef,
+             Date<=data$Date) %>%
+      dplyr::summarise(Vmax=max(FFM),
+                Vsum=mean(FFM))%>%
+      dplyr::mutate(Vsum=Vsum*data$T_Q)
+    return(res)
+  }
+  result=result %>%
+    dplyr::mutate(QVdata=purrr::map(data,f_period))%>%
+    tidyr::unnest(cols=c(data,QVdata)) %>%
+    dplyr::select(-Date,-Date_bef)
   return(result)
 }
 
-complete_Qdata_with_Vdata=function(Qdata,Vdata){
-  result=Qdata %>%
-    group_by(site) %>%
-    nest(Qdata=-site) %>%
-    mutate(Vdata=purrr::map(site,~filter(Vdata,site==.x))) %>%
-    mutate(Qdatac=purrr::map2(Qdata,
-                              Vdata,
-                              complete_Qdata_with_Vdata_onesite))  %>%
-    unnest(cols=c(site,Qdatac)) %>%
-    ungroup()
-  return(result)
-}
