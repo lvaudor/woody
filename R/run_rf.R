5ddb0a224d486a3af93eeede9940062a65a35e97
lvaudor <lise.vaudor@ens-lyon.fr>
2021-01-28 08:31:17 +0100

    add predictor variables of choice

--- R/run_rf.R
+++ R/run_rf.R
@@ -1,15 +1,13 @@
 #' runs random forest model
-#' @param Wdatc_wt data with wood data as waiting times and variables Q, rT_Q, S, Y
+#' @param Wdatc_wt data with wood data as waiting times
+#' @param pred_vars predictor variables by default c("Q","rT_Q","S")
 #' @export
 #' @return random forest object
 #' @examples
-
-run_rf=function(Wdatc_wt){
-  Wdata_rf <- Wdatc_wt %>%
-    dplyr::select(Q,
-                  rT_Q,
-                  S,
-                  Y) %>%
+run_rf=function(Wdatcwt,pred_vars=c("Q","S","rT_Q")){
+  all_vars=c(pred_vars,"Y")
+  Wdata_rf <- Wdatcwt %>%
+    dplyr::select(all_vars) %>%
     na.omit()
   myrf=randomForest::randomForest(Y~.,data=Wdata_rf)
   return(myrf)
