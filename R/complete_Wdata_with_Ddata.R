50e38550b60ab30029a8fab74ada2aa4eecb5ca0
lvaudor <lise.vaudor@ens-lyon.fr>
2021-03-09 09:04:42 +0100

    de-tidyeval var approx

--- R/complete_Wdata_with_Ddata.R
+++ R/complete_Wdata_with_Ddata.R
@@ -5,24 +5,20 @@
 #' @return Wdata completed with variables Q, T_Q, and S and rT_Q
 #' @examples
 complete_Wdata_with_Ddata=function(Wdata,Ddata, newvars=c("Q","T_Q","S","rT_Q")){
-  {
-    result=Wdata
-    for(var in newvars){
-      varfactor=class(Ddata[[var]])!="numeric"
-      if(varfactor){
-        levs=levels(as.factor(Ddata[[var]]))
-        Ddata=Ddata %>%
-          dplyr::mutate(!!var:=as.numeric(!!var))
-      }
-      result=result %>%
-        dplyr::mutate(!!var:=approx(Ddata$Time,
-                                    Ddata[[var]],
-                                    xout=Wdata$Time)$y)
-      if(varfactor){
-        result=result %>%
-          dplyr::mutate(!!var:=levs[!!var])
-      }
+    result = Wdata
+    for (var in newvars) {
+        varfactor = class(Ddata[[var]]) != "numeric"
+        if (varfactor) {
+            levs = levels(as.factor(Ddata[[var]]))
+            Ddata[[var]] = as.numeric(Ddata[[var]])
+        }
+        result[[var]] = approx(Ddata$Time,
+                               Ddata[[var]],
+                               xout = Wdata$Time)$y
+        if (varfactor) {
+            result[[var]] = factor(levs[result[[var]]],
+                                   levels=levs)
+        }
     }
     return(result)
-  }
 }
