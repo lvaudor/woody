6f8033422cf7218aa9160dcac6d1b8f38590112b
lvaudor <lise.vaudor@ens-lyon.fr>
2022-01-25 10:01:05 +0100

    consider length when importing Wdata

--- R/import_Wdata.R
+++ R/import_Wdata.R
@@ -1,11 +1,15 @@
 #' Imports data regarding occurrence of wood from files under path (path>1 directory=1 event>all logs of the event)
 #' @param path the path towards the events directory
 #' @param site the name of the site considered
+#' @param min_length the minimum length for a wood piece to be included in the dataset. Defaults to NA (no filtering).
+#' @param sample_length whether to sample the wood pieces with no length provided, according to the proportion of pieces with Length>min_length. Defaults to FALSE.
 #' @importFrom magrittr %>%
 #' @export
 #' @return Wdata
 #' @example
-#' Wdata=import_Wdata("data-raw/wood_data_Ain", site="Ain")
+#' Wdata=import_Wdata("data-raw/wood_data_Ain", site="Ain", min_length=NA, sample_length=FALSE)
+#' Wdata=import_Wdata("data-raw/wood_data_Allier", site="Allier", min_length=NA, sample_length=FALSE)
+
 import_Wdata=function(path,site=path){
     if(!stringr::str_detect(path,"\\/$")){path=stringr::str_c(path,"/")}
     event_dir=paste0(path,list.files(path))
@@ -24,5 +28,23 @@ import_Wdata=function(path,site=path){
           dplyr::select(-maDate) %>%
           tidyr::unite("sitevent",site,event,remove=FALSE) %>%
           dplyr::select(site,event,sitevent,everything())
+    if(!is.na(min_length)){
+      Wdata_lp=Wdata %>%
+        dplyr::mutate(length_provided=!is.na(Length))
+      Wdata_li=Wdata_lp %>%
+        dplyr::filter(length_provided) %>%
+        dplyr::filter(Length > min_length)
+      if(sample_length){
+      # do not remove all rows with missing length
+          prop=nrow(Wdata_li)/nrow(Wdata_lp)
+          Wdata_ls=Wdata_lp %>%
+            dplyr::filter(!length_provided)
+          nslice=floor(nrow(Wdata_ls)*prop)
+          Wdata_ls=Wdata_ls %>%
+            dplyr::slice(n=nslice) %>%
+            dplyr::select(-length_provided)
+          Wdata=dplyr::bind_rows(Wdata_li,Wdata_ls) %>%
+            dplyr::arrange(Time)
+    }
   return(Wdata)
 }
--- R/read_1_wood_log_file.R
+++ R/read_1_wood_log_file.R
@@ -1,10 +1,13 @@
 #' Reads one wood log file. To be used from import_Wdata()
 #' @param file the path to one log file
 #' @return the data from file
+#' @examples
+#' woody:::read_1_wood_log_file("data-raw/wood_data_Ain/event20071122/Wood Log 20071122 15h11m.txt")
+#' woody:::read_1_wood_log_file("data-raw/wood_data_Ain/event20120101/Event_20120101.xlsx")
 read_1_wood_log_file=function(file){
   if(stringr::str_sub(file,-5)==".xlsx"){
     result=readxl::read_excel(file) %>%
-      dplyr::select(Time)
+      dplyr::select(Time,Length)
     }
   if(stringr::str_sub(file,-4)==".txt"){
     result=readr::read_table2(file,skip=1) %>%
@@ -12,7 +15,7 @@ read_1_wood_log_file=function(file){
                                 " ",
                                 as.character(Time))) %>%
       dplyr::mutate(Time=lubridate::ymd_hms(Time)) %>%
-      dplyr::select(Time)
+      dplyr::select(Time,Length=`Length(m)`)
   }
   return(result)
 }
