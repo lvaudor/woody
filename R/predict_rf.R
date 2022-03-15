94738d5ed142ab7ee2e88ac505a482faa133389a
lvaudor <lise.vaudor@ens-lyon.fr>
2022-03-15 14:24:43 +0100

    correct covariate shift and sigma=f(mu)

--- R/predict_rf.R
+++ R/predict_rf.R
@@ -2,10 +2,11 @@
 #
 #' @param data data with variables Q, rT_Q, S
 #' @param obj_rf random forest object
+#' @param correction whether to correct covariate shift, in which case two coefficients (alpha,beta) should be provided. Defaults to (1,0) i.e. no correction.
 #' @export
 #' @return Wdata completed with column Ypred (log of hourly flux) and Npred (hourly flux)
 
-predict_rf=function(newdata,obj_rf){
+predict_rf=function(newdata,obj_rf, correction=c(1,0)){
   # make sure newdata has same factor levels as original data
   # if one of the predictors is indeed a factor
   nlevs=purrr::map_dbl(myrf$forest$xlevels,length)
@@ -17,6 +18,8 @@ predict_rf=function(newdata,obj_rf){
   }
 
   newdata=newdata %>%
-    dplyr::mutate(Y_pred=predict(object=obj_rf,newdata=newdata))
+    dplyr::mutate(Y_pred=predict(object=obj_rf,newdata=newdata)) %>%
+    dplyr::mutate(Y_pred=correction[1]*Y_pred+correction[2])
+
   return(newdata)
 }
--- R/summarise_Qdata.R
+++ R/summarise_Qdata.R
@@ -4,19 +4,20 @@
 #' @param sigma the residual error in the estimates of Ypred
 #' @export
 #' @return summarised data
-summarise_Qdata=function(Qdata,sigma){
+summarise_Qdata=function(Qdata,sigma_coeffs){
   result=Qdata %>%
-        mutate(Time = lubridate::floor_date(Time, "hour")) %>%
-        group_by(site,station,Time) %>%
-        summarise(n=n(),
+        dplyr::mutate(Time = lubridate::floor_date(Time, "hour")) %>%
+        dplyr::group_by(site,station,Time) %>%
+        dplyr::summarise(n=n(),
                   Q=mean(Q),
                   rT_Q=mean(rT_Q),
                   S=mean(S),
                   T_Q=mean(T_Q),
-                  Y_pred=mean(Y_pred), # 3600/W follows a log-normal law of mean mean(Ypred)
-                  N_pred=exp(log(3600)-Y_pred+(sigma^2)/2),
-                  N_pred=3600/N_pred) %>%
+                  mu=mean(Y_pred),
+                  Y_pred=mu) %>% # 3600/W follows a log-normal law of mean mu=mean(Ypred),
+    mutate(sigma=sigma_coeffs[1]*mu+sigma_coeffs[2],
+           N_pred=exp(mu-(sigma^2)/2)) %>%
     unique() %>%
-    ungroup()
+    dplyr::ungroup()
   return(result)
 }
